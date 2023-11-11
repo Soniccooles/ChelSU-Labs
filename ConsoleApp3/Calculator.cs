1b1fb0cce82b300bacbbc75364e328b28f07aeba
Soniccooles <andrey.sonic@mail.ru>
2023-11-11 22:22:24 +0500

    Added RPN

--- ConsoleApp3/Calculator.cs
+++ ConsoleApp3/Calculator.cs
@@ -4,6 +4,8 @@ using System.ComponentModel.Design;
 using System.Dynamic;
 using System.Globalization;
 using System.Linq.Expressions;
+using System.Numerics;
+using System.Reflection.Metadata;
 using System.Reflection.Metadata.Ecma335;
 using System.Threading.Tasks.Dataflow;
 
@@ -19,22 +21,22 @@ public class Programm // Класс программ qqqeqeqeq
             // Console.WriteLine(string.Join(' ', WriteOperands(input)));
             // Console.WriteLine(string.Join(' ', WriteNumbers(input)));
             // Console.WriteLine(WriteResult(input));
-            Console.WriteLine(string.Join(' ', ToRPN(Parse(input))));
+            Console.WriteLine(string.Join(' ', Calculate(ToRPN(Parse(input)))));
         }
     }
     public static List<object> Parse(string input)
     {
-
         List<object> list = new List<object>();
         string number = "";
         foreach (var element in input + ' ')
         {
-            if (Char.IsDigit(element))
+
+            if (Char.IsDigit(element) && (element != '(' || element != ')') || (element == ','))
             {
                 number += element;
             }
 
-            else
+            else if (number != "")
             {
                 list.Add(float.Parse(number));
                 number = "";
@@ -43,15 +45,13 @@ public class Programm // Класс программ qqqeqeqeq
             if (element == '*' || element == '/' || element == '+' || element == '-' || element == '(' || element == ')')
             {
                 list.Add(element);
+                continue;
             }
         }
         return list;
     }
     public static List<object> ToRPN(List<object> newInput)
     {
-        int token1 = 0;
-        int token2 = 0;
-        string number = "";
         List<object> result = new List<object>();
         Stack<char> stack = new Stack<char>();
         Console.WriteLine(string.Join(',', newInput));
@@ -72,6 +72,8 @@ public class Programm // Класс программ qqqeqeqeq
                 {
                     result.Add(stack.Pop());
                 }
+                if (stack.Peek() == '(')
+                    stack.Pop();
             }
             if (Convert.ToChar(newInput[elem]) == '*' || Convert.ToChar(newInput[elem]) == '/')
             {
@@ -79,15 +81,46 @@ public class Programm // Класс программ qqqeqeqeq
             }
             if (Convert.ToChar(newInput[elem]) == '+' || Convert.ToChar(newInput[elem]) == '-')
             {
-                stack.Push(Convert.ToChar(elem));
+                if (stack.Count != 0)
+                {
+                    if (stack.Peek() == '*' || stack.Peek() == '/')
+                    {
+                        result.Add(stack.Pop());
+                    }
+                }
+                stack.Push(Convert.ToChar(newInput[elem]));
             }
         }
-        Console.WriteLine(string.Join(',', stack));
+        while (stack.Contains('+') || stack.Contains('-') || stack.Contains('*') || stack.Contains('/'))
+        {
+            result.Add(stack.Pop());
+        }
         return result;
     }
-    public static float Calculate(List<object> resulte)
+    public static float Calculate(List<object> result)
     {
-        float nichego = 1;
-        return nichego;
+        int index = 0;
+        for (int i = 0; i <= result.Count; i++)
+        {
+            if (Convert.ToChar(result[i]) == '*' || Convert.ToChar(result[i]) == '/' || Convert.ToChar(result[i]) == '+' ||
+                Convert.ToChar(result[i]) == '-')
+            {
+                index = i;
+                break;
+            }
+        }
+
+        float num1 = (float) result[index-2];
+        float num2 = (float) result[index-1];
+        char op = Convert.ToChar(result[index]);
+        float finalResult = 0;
+        switch (op)
+        {
+            case '*': return num1 * num2;
+            case '/': return num1 / num2;
+            case '+': return num1 + num2;
+            case '-': return num1 - num2;
+        }
+        return finalResult;
     }
 }
